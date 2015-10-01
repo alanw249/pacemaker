9919ae89d63ed6bd68b3ef622c1a13c9ce8bf94f
alanw249 <alanw249@hotmail.com>
2015-10-01 11:57:05 +0100

    New Features Added - XStream Internal repository

--- src/controllers/Main.java
+++ src/controllers/Main.java
@@ -2,23 +2,38 @@ package controllers;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
 
 import utils.FileLogger;
 import models.User;
 
+@SuppressWarnings("unused")
 public class Main
 {
-	 public static void main(String[] args)
-	  {
-	    FileLogger logger = FileLogger.getLogger();
-	    logger.log("Creating user list");
+	public static void main(String[] args) throws IOException
+  {
+    FileLogger logger = FileLogger.getLogger();
+    logger.log("Creating user list");
 
-	    List<User> users = new ArrayList<User>();
-	    users.add(new User("Bart", "Simpson", "bart@simpson.com", "secret"));
-	    users.add(new User("Homer", "Simpson", "bart@simpson.com", "secret"));
-	    users.add(new User("Lisa", "Simpson", "bart@simpson.com", "secret"));
-	    System.out.println(users);
+    List<User> users = new ArrayList<User>();
+    users.add(new User("Bart", "Simpson", "bart@simpson.com", "secret"));
+    users.add(new User("Homer", "Simpson", "bart@simpson.com", "secret"));
+    users.add(new User("Lisa", "Simpson", "bart@simpson.com", "secret"));
+    System.out.println(users);
 
-	    logger.log("Finished - shutting down");
-	  }
+    logger.log("Serializing contacts to XML");
+    XStream xstream = new XStream(new DomDriver());
+    ObjectOutputStream out = xstream.createObjectOutputStream(new FileWriter("users.xml"));
+    out.writeObject(users);
+    out.close();    
+
+    logger.log("Finished - shutting down");
+  }
 }
