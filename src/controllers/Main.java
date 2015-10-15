2337363318aae51cdc48f760f4b3d1fa80a44fa3
alanw249 <alanw249@hotmail.com>
2015-10-15 12:04:47 +0100

    Implemented Id structure
    Signed-off-by: alanw249 <alanw249@hotmail.com>

--- src/controllers/Main.java
+++ src/controllers/Main.java
@@ -13,12 +13,19 @@ public class Main
   {    
     PaceMakerAPI pacemakerAPI = new PaceMakerAPI();
 
-    pacemakerAPI.createUser("Bart",  "Simpson", "bart@simpson.com",  "secret");
-    pacemakerAPI.createUser("Homer", "Simpson", "homer@simpson.com", "secret");
-    pacemakerAPI.createUser("Lisa",  "Simpson", "lisa@simpson.com",  "secret");
+    pacemakerAPI.createUser("Bart", "Simpson",   "bart@simpson.com", "secret");
+    pacemakerAPI.createUser("Homer", "Simpson",  "homer@simpson.com", "secret");
+    pacemakerAPI.createUser("Lisa", "Simpson", " lisa@simpson.com", "secret");
 
-    Collection<User> user = pacemakerAPI.getUsers();
-    System.out.println(user);
+    Collection<User> users = pacemakerAPI.getUsers();
+    System.out.println(users);
+
+    User homer = pacemakerAPI.getUserByEmail("homer@simpson.com");
+    System.out.println(homer);
+
+    pacemakerAPI.deleteUser(homer.id);
+    users = pacemakerAPI.getUsers();
+    System.out.println(users);
   }
   
 }
