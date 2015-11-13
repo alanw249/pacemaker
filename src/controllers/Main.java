f51ee2ba396f1e56d204b274549019824073e8dd
alanw249 <alanw249@hotmail.com>
2015-11-13 11:38:16 +0000

    Introduced XML database

--- src/controllers/Main.java
+++ src/controllers/Main.java
@@ -1,25 +1,28 @@
 package controllers;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.util.Collection;
 import java.util.List;
+
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
 
 import static com.google.common.base.MoreObjects.toStringHelper;
 import models.User;
 
 public class Main
 {
-  public static void main(String[] args) throws IOException
+  public static void main(String[] args) throws Exception
   {    
     PaceMakerAPI pacemakerAPI = new PaceMakerAPI();
-
-    pacemakerAPI.createUser("Bart", "Simpson",   "bart@simpson.com", "secret");
-    pacemakerAPI.createUser("Homer", "Simpson",  "homer@simpson.com", "secret");
-    pacemakerAPI.createUser("Lisa", "Simpson", " lisa@simpson.com", "secret");
-
+    File file = new File("datastore.xml");
+    pacemakerAPI.load(file);
     Collection<User> users = pacemakerAPI.getUsers();
     System.out.println(users);
-
+    
     User homer = pacemakerAPI.getUserByEmail("homer@simpson.com");
     System.out.println(homer);
 
