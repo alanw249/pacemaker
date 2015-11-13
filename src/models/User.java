f6433227c97c460f07a0a4ae739bdb30e9570aa4
alanw249 <alanw249@hotmail.com>
2015-11-13 09:52:55 +0000

    Added User Testing

--- src/models/User.java
+++ src/models/User.java
@@ -35,7 +35,8 @@ public class User
   @Override
   public String toString()
   {
-    return toStringHelper(this).addValue(firstName)
+    return toStringHelper(this).addValue(id)
+    														.addValue(firstName)
                                .addValue(lastName)
                                .addValue(password)
                                .addValue(email)                               
